9b24aa5c5b8575e456dc1ab4d70104ad40627a1c
Bipin <bipinchoubey771@gmail.com>
2025-07-27 22:18:12 +0530

    Added week 6

---
+++ Week6/React-4/blogapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Posts from './Components/Posts';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <main>
+        <Posts />
+      </main>
+    </div>
+  );
+}
+
+export default App;
